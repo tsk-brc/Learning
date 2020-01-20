8f24c6c60f4b838df0cc2f707bdea8b5ec37c5ac
T-o-s-s-h-y <51285386+T-o-s-s-h-y@users.noreply.github.com>
2020-01-20 16:01:52 +0900

    整形

--- Exercises/training_b055.java
+++ Exercises/training_b055.java
@@ -12,34 +12,34 @@ public class Main {
         //入力データを受け取る
         Scanner sc = new Scanner(System.in);
         sc.useDelimiter(" |\\s");
-		//タクシーの台数
+        //タクシーの台数
         int taxiCount = Integer.parseInt(sc.next());
-		//移動距離
+        //移動距離
         int distance = Integer.parseInt(sc.next());
         int[] needMoney = new int[taxiCount];
         for(int i = 0; i < taxiCount; i++){
-		 //初乗り距離
+         //初乗り距離
          int firstDistance = Integer.parseInt(sc.next());
-		 //初乗り運賃
+         //初乗り運賃
          int firstPay = Integer.parseInt(sc.next());
-		 //加算距離
+         //加算距離
          int addDistance = Integer.parseInt(sc.next());
-		 //加算運賃
+         //加算運賃
          int addPay = Integer.parseInt(sc.next());
-		 //現在までの移動距離
+         //現在までの移動距離
          int totalDistance = firstDistance;
-		 //現在までの料金
+         //現在までの料金
          int totalPay = firstPay;
-		 //移動距離を超えるまで加算運賃を追加する
+         //移動距離を超えるまで加算運賃を追加する
          while(distance >= totalDistance){
            totalDistance += addDistance;
            totalPay += addPay;
          }
          needMoney[i] = totalPay;
         }
-		//料金を昇順でソートする
+        //料金を昇順でソートする
         Arrays.sort(needMoney);
-		//最安値と最高値を出力する
+        //最安値と最高値を出力する
         System.out.println(needMoney[0] + " " + needMoney[taxiCount - 1]);
     }
 }
