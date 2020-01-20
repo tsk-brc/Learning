2a3da318d5e636451a459cd47531895ff5295339
T-o-s-s-h-y <51285386+T-o-s-s-h-y@users.noreply.github.com>
2020-01-20 15:58:43 +0900

    整形

--- Exercises/training_b073.java
+++ Exercises/training_b073.java
@@ -10,33 +10,33 @@ public class Main {
     public static void main(String[] args) {
 
         //入力データを受け取る
-		Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         //木の本数
         int tree = Integer.parseInt(sc.next());
-		//電球の平均個数の下限
+        //電球の平均個数の下限
         int ave = Integer.parseInt(sc.next());
         int[] num = new int[tree];
-		//現在の各木についている電球の数
+        //現在の各木についている電球の数
         for(int i = 0; i < tree; i++){
           num[i] = Integer.parseInt(sc.next());
         }
-		//調査対象数
+        //調査対象数
         int check = Integer.parseInt(sc.next());
-		//調査対象の数だけループ
+        //調査対象の数だけループ
         for(int j = 0; j < check; j++){
-		  //調査対象の始点
+          //調査対象の始点
           int start = Integer.parseInt(sc.next());
-		  //調査対象の終点
+          //調査対象の終点
           int end = Integer.parseInt(sc.next());
-		  //調査対象の電球数
+          //調査対象の電球数
           int aveNum = 0;
-		  //調査対象の木の本数
+          //調査対象の木の本数
           int aveCount = 0;
           for(int k = start - 1; k < end; k++){
             aveNum += num[k];
             aveCount++;
           }
-		  //電球の平均個数の下限を下回っている間、調査対象の各木に電球を1つずつ追加する
+          //電球の平均個数の下限を下回っている間、調査対象の各木に電球を1つずつ追加する
           while(ave > aveNum / aveCount){
             aveNum = 0;
             for(int m = start - 1; m < end; m++){
@@ -45,7 +45,7 @@ public class Main {
             }
           }
         }
-		//最終的な各木の電球の個数をスペース区切りで出力する
+        //最終的な各木の電球の個数をスペース区切りで出力する
         StringBuilder sb = new StringBuilder();
         for(int n = 0; n < tree; n++){
           if(n != 0){
