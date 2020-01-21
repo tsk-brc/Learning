117e32291f5939f5d3045b4c6e0c372fade6e8d3
T-o-s-s-h-y <51285386+T-o-s-s-h-y@users.noreply.github.com>
2020-01-21 09:36:34 +0900

    整形

--- Exercises/mission_c_006.java
+++ Exercises/mission_c_006.java
@@ -14,35 +14,35 @@ public class Main {
         //入力データを受け取る
         Scanner sc = new Scanner(System.in);
         sc.useDelimiter(" |\\s");
-		//アイテムの個数
+        //アイテムの個数
         int items = Integer.parseInt(sc.next());
         //プレイヤー数
-		int player = Integer.parseInt(sc.next());
+        int player = Integer.parseInt(sc.next());
         //出力対象になるランキング
-		int rank = Integer.parseInt(sc.next());
+        int rank = Integer.parseInt(sc.next());
         //各アイテムのポイントを格納する配列
-		double[] itemsPoint = new double[items];
-		//各ユーザーのスコアを格納する配列
+        double[] itemsPoint = new double[items];
+        //各ユーザーのスコアを格納する配列
         int[] playerPoint = new int[player];
-		//アイテムのポイントを配列に格納
+        //アイテムのポイントを配列に格納
         for(int i =0; i< items; i++){
           itemsPoint[i] = Double.parseDouble(sc.next());
         }
-		//各ユーザーのスコアを計算し、配列に格納
+        //各ユーザーのスコアを計算し、配列に格納
         for(int j = 0; j < player; j++){
           double point = 0.0;
           for(int k = 0; k < items; k++){
               point += itemsPoint[k] * Integer.parseInt(sc.next());
           }
           BigDecimal bd = new BigDecimal(point);
-		  //小数点以下を四捨五入
+          //小数点以下を四捨五入
           BigDecimal bd1 = bd.setScale(0, BigDecimal.ROUND_HALF_UP); 
           playerPoint[j] = bd1.intValue();
         }
-		//スコアを昇順にソート
+        //スコアを昇順にソート
         Arrays.sort(playerPoint);
         //出力対象以内のランキングのスコアを出力
-		for(int l = player - 1; l > player - rank - 1; l--){
+        for(int l = player - 1; l > player - rank - 1; l--){
           System.out.println(playerPoint[l]);
         }
     }
