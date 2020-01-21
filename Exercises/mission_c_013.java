264c88e63dfafc196bcfdc8ed808356fa2be3d75
T-o-s-s-h-y <51285386+T-o-s-s-h-y@users.noreply.github.com>
2020-01-21 09:38:47 +0900

    整形

--- Exercises/mission_c_013.java
+++ Exercises/mission_c_013.java
@@ -9,21 +9,21 @@ public class Main {
 
         //入力データを受け取る
         Scanner sc = new Scanner(System.in);
-		//嫌いな数字
+　　　　　//嫌いな数字
         String unlikeNum = sc.nextLine();
         //部屋の数
-		int room = Integer.parseInt(sc.nextLine());
+        int room = Integer.parseInt(sc.nextLine());
         boolean checkFlg = true;
         for(int i = 0; i < room; i++){
           //部屋番号
-		  String target = sc.nextLine();
-		  //部屋番号に嫌いな数字が含まれているかどうかをチェックする
+          String target = sc.nextLine();
+          //部屋番号に嫌いな数字が含まれているかどうかをチェックする
           if(!target.contains(unlikeNum)){
             System.out.println(target);
             checkFlg = false;
           }
         }
-		//住める部屋がない場合はnoneを出力
+        //住める部屋がない場合はnoneを出力
         if(checkFlg){
           System.out.println("none");
         }
