292cbc61769321c2bac97d3439791efc9438b426
T-o-s-s-h-y <51285386+T-o-s-s-h-y@users.noreply.github.com>
2020-01-27 11:55:39 +0900

    問題の追加

---
+++ Exercises/mission_d_111.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+/*
+  表示可能な文字数までを表示する。
+*/
+public class Main {
+    public static void main(String[] args) {
+
+        //入力データを受け取る
+        Scanner sc = new Scanner(System.in);
+        //表示可能な文字数
+        int num = Integer.parseInt(sc.nextLine());
+        //表示させる文字
+        String str = sc.nextLine();
+        //表示可能な文字数まで表示
+        System.out.println(str.substring(0,num));
+    }
+}
