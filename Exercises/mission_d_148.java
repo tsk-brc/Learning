8e15925a6e93a920034ca741848dfd471aa7c4f3
T_o_s_s_h_y <tosshy_1219@outlook.jp>
2020-05-05 14:42:57 +0900

    問題の追加

---
+++ Exercises/mission_d_148.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+/*
+  試験の点数を算出する。
+  試験は2つの科目からなり、試験1で一定以上の点数を取らないと足切りとなり、
+  試験2の点数は0点として採点される。
+*/
+public class Main {
+    public static void main(String[] args) {
+
+        //入力データを受け取る
+        Scanner sc = new Scanner(System.in);
+        //試験1の得点
+        int num1 = Integer.parseInt(sc.next());
+        //試験2の得点
+        int num2 = Integer.parseInt(sc.next());
+        //試験1の足切りの点数
+        int num3 = Integer.parseInt(sc.next());
+        //試験の点を算出
+        if(num1 < num3){
+          System.out.println(num1);
+        } else {
+          System.out.println(num1 + num2);
+        }
+    }
+}
