fde985df5ce76c95b0510cf0d5da62b3a4d7c35b
T_o_s_s_h_y <tosshy_1219@outlook.jp>
2020-05-10 02:54:59 +0900

    新規追加

---
+++ indivisual/Ruby/cherry_book/lib/rgb.rb
@@ -0,0 +1,14 @@
+def to_hex(r,g,b)
+  hex = '#'
+  [r, g, b].each do |n|
+    hex += n.to_s(16).rjust(2,'0')
+  end
+  hex
+end
+
+def to_ints(hex)
+  r, g, b = hex[1..2], hex[3..4], hex[5..6]
+  [r, g, b].map do |s|
+    s.hex
+  end
+end
