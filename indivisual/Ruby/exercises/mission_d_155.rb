8a2095416f87d39825deac00abe9aef3ccea7763
T_o_s_s_h_y <tosshy_1219@outlook.jp>
2020-06-20 20:50:01 +0900

    問題の追加

---
+++ indivisual/Ruby/exercises/mission_d_155.rb
@@ -0,0 +1,19 @@
+=begin
+  入場料を計算する。
+=end
+# 入場する人数
+person = gets.to_i
+# 指定されたリスト[団体料金が適用される人数の下限 団体料金 個人料金]
+show_list = gets
+# 空白区切りで配列に格納
+list = show_list.split
+# 合計金額
+money = 0
+# 団体料金が適用されるか
+if person >= list[0].to_i
+   money = person * list[1].to_i
+else
+   money = person * list[2].to_i
+end
+# 結果を出力
+puts money
