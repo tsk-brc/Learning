3c0b98b05ddca8138d10abfd64842d1f45d8468d
T_o_s_s_h_y <tosshy_1219@outlook.jp>
2020-09-26 23:14:03 +0900

    問題の追加

--- indivisual/Ruby/exercises/app/mission_d_156.rb
+++ indivisual/Ruby/exercises/app/mission_d_156.rb
@@ -2,9 +2,14 @@
   密を避けるために必要な面積を求める。
 =end
 
-# 1人あたりに必要な面積
-square = gets.to_i
-# 人数
-person = gets.to_i
-# 必要な面積を算出
-puts square * person
+class MissionD156
+
+  def run
+    # 1人あたりに必要な面積
+    square = STDIN.gets.to_i
+    # 人数
+    person = STDIN.gets.to_i
+    # 必要な面積を算出
+    puts square * person
+  end
+end
---
+++ indivisual/Ruby/exercises/spec/mission_d_156_spec.rb
@@ -0,0 +1,20 @@
+require './app/mission_d_156.rb'
+
+describe '面積算出チェック' do
+  describe '出力結果のテスト' do
+
+    let(:missionD156) { MissionD156.new }
+
+    it 'テスト1' do
+      allow(STDIN).to receive(:gets).and_return "4\n", "15\n"
+      expect(STDOUT).to receive(:puts).with(60)
+      missionD156.run
+    end
+
+    it 'テスト2' do
+      allow(STDIN).to receive(:gets).and_return "3\n", "12\n"
+      expect(STDOUT).to receive(:puts).with(36)
+      missionD156.run
+    end
+  end
+end
