7859bc6b6ec63821b46d965c841d845d4452c997
T_o_s_s_h_y <tosshy_1219@outlook.jp>
2020-09-26 22:01:40 +0900

    テスト修正

--- indivisual/Ruby/exercises/spec/mission_d_162_spec.rb
+++ indivisual/Ruby/exercises/spec/mission_d_162_spec.rb
@@ -6,13 +6,13 @@ describe '重量計算' do
     let(:missionD162) { MissionD162.new }
 
     it 'テスト1' do
-      allow(STDIN).to receive(:gets) { '3\n' }
+      allow(STDIN).to receive(:gets) { "3\n" }
       expect(STDOUT).to receive(:puts).with(450)
       missionD162.run
     end
 
     it 'テスト2' do
-      allow(STDIN).to receive(:gets) { '1\n' }
+      allow(STDIN).to receive(:gets) { "1\n" }
       expect(STDOUT).to receive(:puts).with(150)
       missionD162.run
     end
