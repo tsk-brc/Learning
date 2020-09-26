b7b32a612d766d40638fb6964a6672d8754b88d4
T_o_s_s_h_y <tosshy_1219@outlook.jp>
2020-09-26 23:53:03 +0900

    テストの追加

--- indivisual/Ruby/exercises/app/mission_d_153.rb
+++ indivisual/Ruby/exercises/app/mission_d_153.rb
@@ -2,9 +2,14 @@
   2番目に値の小さい数字を出力する。
 =end
 
-# 値を受け取る
-nums = gets
-# 数値としてソート
-sort_nums = nums.split.sort{|a, b| a.to_i <=> b.to_i }
-# 2番目の値を出力
-puts sort_nums[1]
+class MissionD153
+
+  def run
+    # 値を受け取る
+    nums = STDIN.gets
+    # 数値としてソート
+    sort_nums = nums.split.sort{|a, b| a.to_i <=> b.to_i }
+    # 2番目の値を出力
+    puts sort_nums[1].to_i
+  end
+end
---
+++ indivisual/Ruby/exercises/spec/mission_d_153_spec.rb
@@ -0,0 +1,20 @@
+require './app/mission_d_153.rb'
+
+describe '数値大小チェック' do
+  describe '出力結果のテスト' do
+
+    let(:missionD153) { MissionD153.new }
+
+    it 'テスト1' do
+      allow(STDIN).to receive(:gets) { "100 200 300\n" }
+      expect(STDOUT).to receive(:puts).with(200)
+      missionD153.run
+    end
+
+    it 'テスト2' do
+      allow(STDIN).to receive(:gets) { "690 342 960\n" }
+      expect(STDOUT).to receive(:puts).with(690)
+      missionD153.run
+    end
+  end
+end
