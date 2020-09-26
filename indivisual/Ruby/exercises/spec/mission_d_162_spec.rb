6fb030f908442cf6746786d7ffd67aee45eecf09
T_o_s_s_h_y <tosshy_1219@outlook.jp>
2020-09-26 21:05:03 +0900

    テスト追加

---
+++ indivisual/Ruby/exercises/spec/mission_d_162_spec.rb
@@ -0,0 +1,20 @@
+require './app/mission_d_162.rb'
+
+describe '重量計算' do
+  describe '出力結果のテスト' do
+
+    let(:missionD162) { MissionD162.new }
+
+    it 'テスト1' do
+      allow(STDIN).to receive(:gets) { '3\n' }
+      expect(STDOUT).to receive(:puts).with(450)
+      missionD162.run
+    end
+
+    it 'テスト2' do
+      allow(STDIN).to receive(:gets) { '1\n' }
+      expect(STDOUT).to receive(:puts).with(150)
+      missionD162.run
+    end
+  end
+end
