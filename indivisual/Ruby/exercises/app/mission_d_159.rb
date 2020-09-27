63630612dfd5e60045f0754c8cc57f79e2a4af3a
T_o_s_s_h_y <tosshy_1219@outlook.jp>
2020-09-27 14:39:42 +0900

    問題の追加

---
+++ indivisual/Ruby/exercises/app/mission_d_159.rb
@@ -0,0 +1,21 @@
+=begin
+  文字の最大長を求める。
+=end
+
+class MissionD159
+
+  def run
+    # 各文字の文字数を取得
+    str1_length = STDIN.gets.chomp.length
+    str2_length = STDIN.gets.chomp.length
+    str3_length = STDIN.gets.chomp.length
+    # 文字数比較
+    if str1_length >= str2_length and str1_length >= str3_length
+      puts str1_length
+    elsif str2_length >= str3_length
+      puts str2_length
+    else
+      puts str3_length
+    end
+  end
+end
---
+++ indivisual/Ruby/exercises/spec/mission_d_159_spec.rb
@@ -0,0 +1,20 @@
+require './app/mission_d_159.rb'
+
+describe '最大文字数チェック' do
+  describe '出力結果のテスト' do
+
+    let(:missionD159) { MissionD159.new }
+
+    it 'テスト1' do
+      allow(STDIN).to receive(:gets).and_return "engineer\n", "coding\n", "programming\n"
+      expect(STDOUT).to receive(:puts).with(11)
+      missionD159.run
+    end
+
+    it 'テスト2' do
+      allow(STDIN).to receive(:gets).and_return "abc\n", "def\n", "ghi\n"
+      expect(STDOUT).to receive(:puts).with(3)
+      missionD159.run
+    end
+  end
+end
