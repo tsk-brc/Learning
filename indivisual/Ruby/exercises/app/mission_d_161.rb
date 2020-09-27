7f78c3bd5a68d2e63171f57f1f037656775b59ae
T_o_s_s_h_y <tosshy_1219@outlook.jp>
2020-09-27 13:38:23 +0900

    問題の追加

---
+++ indivisual/Ruby/exercises/app/mission_d_161.rb
@@ -0,0 +1,23 @@
+=begin
+  1週間の降水量で貯められる水の量を調査する。
+=end
+
+class MissionD161
+
+  def run
+    # 許容できる最大量
+    maxNum = STDIN.gets.to_i
+    # 1週間の降水量
+    total = 0
+    # 各日の降水量を加算
+    7.times {
+      total += STDIN.gets.to_i
+    }
+    # 貯水量を出力
+    if maxNum >= total
+      puts total
+    else
+      puts maxNum
+    end
+  end
+end
---
+++ indivisual/Ruby/exercises/spec/mission_d_161_spec.rb
@@ -0,0 +1,20 @@
+require './app/mission_d_161.rb'
+
+describe '面積算出チェック' do
+  describe '出力結果のテスト' do
+
+    let(:missionD161) { MissionD161.new }
+
+    it 'テスト1' do
+      allow(STDIN).to receive(:gets).and_return "100\n", "1\n", "2\n", "3\n", "4\n", "5\n", "6\n", "7\n"
+      expect(STDOUT).to receive(:puts).with(28)
+      missionD161.run
+    end
+
+    it 'テスト2' do
+      allow(STDIN).to receive(:gets).and_return "200\n", "10\n", "20\n", "30\n", "40\n", "50\n", "60\n", "70\n"
+      expect(STDOUT).to receive(:puts).with(200)
+      missionD161.run
+    end
+  end
+end
