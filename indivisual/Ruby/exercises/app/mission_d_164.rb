34afe93559b069c2fcb3c2a8143029301498c1e9
T_o_s_s_h_y <tosshy_1219@outlook.jp>
2020-09-27 15:32:06 +0900

    問題の追加

---
+++ indivisual/Ruby/exercises/app/mission_d_164.rb
@@ -0,0 +1,23 @@
+=begin
+   2のn乗で割れる数字かどうかをチェックする。
+=end
+
+class MissionD164
+
+  def run
+    # 対象の数字
+    num = STDIN.gets.to_i
+    # 値が1になるまで2で割り続ける
+    while num > 1 do 
+      if num % 2 == 0
+        num /= 2
+        if num == 1
+          puts 'OK'
+        end
+      else
+        puts 'NG'
+        break
+      end
+    end
+  end
+end
---
+++ indivisual/Ruby/exercises/spec/mission_d_164_spec.rb
@@ -0,0 +1,20 @@
+require './app/mission_d_164.rb'
+
+describe 'べき乗チェック' do
+  describe '出力結果のテスト' do
+
+    let(:missionD164) { MissionD164.new }
+
+    it 'テスト1' do
+      allow(STDIN).to receive(:gets) { "256\n" }
+      expect(STDOUT).to receive(:puts).with('OK')
+      missionD164.run
+    end
+
+    it 'テスト2' do
+      allow(STDIN).to receive(:gets) { "255\n" }
+      expect(STDOUT).to receive(:puts).with('NG')
+      missionD164.run
+    end
+  end
+end
