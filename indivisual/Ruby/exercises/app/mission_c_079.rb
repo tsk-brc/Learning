3b9fc37a53a7e82ab0e49d92c45a3aa0bf2085fd
T_o_s_s_h_y <tosshy_1219@outlook.jp>
2020-09-27 22:29:32 +0900

    問題の追加

---
+++ indivisual/Ruby/exercises/app/mission_c_079.rb
@@ -0,0 +1,35 @@
+=begin
+  トレーディングカードを購入し、何枚目でコンプリートできたかを調査する。
+=end
+class MissionC079 
+
+  def run
+    # カードを手に入れた枚数とカードの種類
+    cardInfo = STDIN.gets.split
+    # 手にしたカードのリスト
+    cardlist = []
+    # コンプリートできなかったか(初期値はカードを1枚も持っていないのでtrue)
+    nonCompFlg = true
+    # 手にしたカードの枚数分だけループ
+    cardInfo[0].to_i.times do |num|
+      # 所持カード枚数を取得
+      list = cardlist.size
+      # カードをリストに格納
+      cardlist[list] = STDIN.gets.to_i
+      # 重複を破棄
+      cardlist.uniq!
+      # カードをコンプリートしたか(重複削除した枚数がカードの種類と一致するか)
+      if cardlist.size == cardInfo[1].to_i
+        # フラグを変更
+        nonCompFlg = false
+        # 何枚目で揃ったかを出力してループを抜ける
+        puts num + 1
+        break
+      end
+    end
+    # 揃わなかったら出力
+    if nonCompFlg
+      puts 'unlucky'
+    end
+  end
+end
---
+++ indivisual/Ruby/exercises/spec/mission_c_079_spec.rb
@@ -0,0 +1,26 @@
+require './app/mission_c_079.rb'
+
+describe 'カードコンプリートのチェック' do
+  describe '出力結果のテスト' do
+
+    let(:missionC079) { MissionC079.new }
+
+    it 'テスト1' do
+      allow(STDIN).to receive(:gets).and_return "6 3\n", "1\n", "2\n", "2\n", "3\n", "1\n", "3\n"
+      expect(STDOUT).to receive(:puts).with(4)
+      missionC079.run
+    end
+
+    it 'テスト2' do
+      allow(STDIN).to receive(:gets).and_return "7 4\n", "1\n", "2\n", "3\n", "1\n", "2\n", "3\n", "1\n"
+      expect(STDOUT).to receive(:puts).with('unlucky')
+      missionC079.run
+    end
+
+    it 'テスト3' do
+      allow(STDIN).to receive(:gets).and_return "1 100\n", "1\n"
+      expect(STDOUT).to receive(:puts).with('unlucky')
+      missionC079.run
+    end
+  end
+end
