35c20c66d372823174fd1e25057a83bf77148d24
T_o_s_s_h_y <tosshy_1219@outlook.jp>
2020-10-01 22:21:51 +0900

    問題の追加

---
+++ indivisual/Ruby/exercises/app/mission_d_166.rb
@@ -0,0 +1,13 @@
+=begin
+  週を日数に変換する。
+=end
+
+class MissionD166
+
+  def run
+    # 週数
+    week = STDIN.gets.to_i
+    # 日数に変換して出力
+    puts week * 7
+  end
+end
---
+++ indivisual/Ruby/exercises/spec/mission_d_166_spec.rb
@@ -0,0 +1,20 @@
+require './app/mission_d_166.rb'
+
+describe '日数変換チェック' do
+  describe '出力結果のテスト' do
+
+    let(:missionD166) { MissionD166.new }
+
+    it 'テスト1' do
+      allow(STDIN).to receive(:gets) { "6\n" }
+      expect(STDOUT).to receive(:puts).with(42)
+      missionD166.run
+    end
+
+    it 'テスト2' do
+      allow(STDIN).to receive(:gets) { "1\n" }
+      expect(STDOUT).to receive(:puts).with(7)
+      missionD166.run
+    end
+  end
+end
