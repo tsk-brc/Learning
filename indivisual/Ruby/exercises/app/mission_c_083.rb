901dd90580220a44e3b0d27b2432ab9f00ddddb8
T_o_s_s_h_y <tosshy_1219@outlook.jp>
2020-10-17 14:34:24 +0900

    問題の追加

---
+++ indivisual/Ruby/exercises/app/mission_c_083.rb
@@ -0,0 +1,33 @@
+=begin
+  売上データを棒グラフで表示する。
+=end
+
+class MissionC083
+
+  def run
+    # 売上データの個数とグラフの表示単位
+    data = STDIN.gets.split
+    # グラフ目盛りの最大値
+    maxNum = 0
+    # 売り上げを格納する配列
+    money = []
+    # データの個数分ループ
+    data[0].to_i.times do |num|
+      # 売り上げの配列に格納
+      money.push(STDIN.gets.to_i / data[1].to_i)
+      # グラフ目盛りの最大値を更新
+      maxNum = money[num] if maxNum < money[num]
+    end
+    # データの個数分ループ
+    data[0].to_i.times do |num2|
+      # 仮データとして'.'で目盛りの最大まで文字列を作成
+      str = '.' * maxNum
+      # 実データ分'*'に置換
+      money[num2].times do |num3|
+        str[num3] = '*'
+      end
+      # 結果を出力
+      puts (num2 + 1).to_s + ':' + str
+    end
+  end
+end
