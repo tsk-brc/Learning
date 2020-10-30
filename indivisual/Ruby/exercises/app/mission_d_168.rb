ab9e97bb6a12916ebe8e5e2a1a8a6720105ff75e
T_o_s_s_h_y <tosshy_1219@outlook.jp>
2020-10-31 08:07:37 +0900

    問題の追加

---
+++ indivisual/Ruby/exercises/app/mission_d_168.rb
@@ -0,0 +1,17 @@
+=begin
+  アメリカ形式で日付を出力する。
+=end
+
+class MissionD168
+
+  def run
+    # 年情報
+    year = STDIN.gets.chomp!
+    # 月情報
+    month = STDIN.gets.chomp!
+    # 日情報
+    day = STDIN.gets.chomp!
+    # アメリカ形式で出力
+    puts month + "/" + day + "/" +  year
+  end
+end
