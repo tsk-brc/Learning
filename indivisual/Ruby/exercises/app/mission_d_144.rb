26d6d5ece3996e70f83a0de51c037f6ca153f5c0
T_o_s_s_h_y <tosshy_1219@outlook.jp>
2020-11-01 22:05:54 +0900

    問題の追加

---
+++ indivisual/Ruby/exercises/app/mission_d_144.rb
@@ -0,0 +1,10 @@
+=begin
+  2進数の数字を10進数に変換する。
+=end
+
+class MissionD144
+
+  def run
+    puts STDIN.gets.to_i(2)
+  end
+end
---
+++ indivisual/Ruby/exercises/spec/mission_d_144_spec.rb
@@ -0,0 +1,20 @@
+require './app/mission_d_144.rb'
+
+describe '2進数変換チェック' do
+  describe '出力結果のテスト' do
+
+    let(:missionD144) { MissionD144.new }
+
+    it 'テスト1' do
+      allow(STDIN).to receive(:gets) { "01011\n" }
+      expect(STDOUT).to receive(:puts).with(11)
+      missionD144.run
+    end
+
+    it 'テスト2' do
+      allow(STDIN).to receive(:gets) { "11111\n" }
+      expect(STDOUT).to receive(:puts).with(31)
+      missionD144.run
+    end
+  end
+end
