7fb6a0f12c2298c5b66c7b3b886f4c5550660a02
T_o_s_s_h_y <tosshy_1219@outlook.jp>
2020-11-03 16:29:13 +0900

    add question

---
+++ indivisual/Ruby/exercises/app/mission_d_143.rb
@@ -0,0 +1,13 @@
+=begin
+  制動距離を計算する。
+=end
+
+class MissionD143
+
+  def run
+    # パラメータ(重さ 速度 ブレーキの強さ)
+    param = STDIN.gets.split
+    # 計算式に当てはめる(L=mv^2/2f)
+    puts param[0].to_i * param[1].to_i ** 2 / (2 * param[2].to_i)
+  end
+end
---
+++ indivisual/Ruby/exercises/spec/mission_d_143_spec.rb
@@ -0,0 +1,20 @@
+require './app/mission_d_143.rb'
+
+describe '制動距離算出チェック' do
+  describe '出力結果のテスト' do
+
+    let(:missionD143) { MissionD143.new }
+
+    it 'テスト1' do
+      allow(STDIN).to receive(:gets) { "100 40 2000\n" }
+      expect(STDOUT).to receive(:puts).with(40)
+      missionD143.run
+    end
+
+    it 'テスト2' do
+      allow(STDIN).to receive(:gets) { "200 15 100\n" }
+      expect(STDOUT).to receive(:puts).with(225)
+      missionD143.run
+    end
+  end
+end
