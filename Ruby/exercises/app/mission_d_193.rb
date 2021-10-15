abb48fdb3f3ac82aa9fe3ef5864bc0dcf2e0e7cc
T-o-s-s-h-y <tosshy_1219@outlook.jp>
2021-10-15 19:07:48 +0900

    add question

---
+++ Ruby/exercises/app/mission_d_193.rb
@@ -0,0 +1,15 @@
+=begin
+  購入した電池で何回交換可能かを調べる。
+=end
+
+class MissionD193
+  def run
+    # 1回の交換で必要な電池数
+    need_batteries  = $stdin.gets.to_i
+    # 購入した電池数
+    bought_batteries = $stdin.gets.to_i
+    # 交換可能な回数と余りの電池数を出力
+    puts bought_batteries / need_batteries
+    puts bought_batteries % need_batteries
+  end
+end
---
+++ Ruby/exercises/spec/mission_d_193_spec.rb
@@ -0,0 +1,21 @@
+require './app/mission_d_193.rb'
+
+describe 'アニバーサリーチェック' do
+  describe '出力結果のテスト' do
+    let(:missionD193) { MissionD193.new }
+
+    it 'テスト1' do
+      allow($stdin).to receive(:gets).and_return "3\n", "10\n"
+      expect($stdout).to receive(:puts).with(3)
+      expect($stdout).to receive(:puts).with(1)
+      missionD193.run
+    end
+
+    it 'テスト2' do
+      allow($stdin).to receive(:gets).and_return "1\n", "1\n"
+      expect($stdout).to receive(:puts).with(1)
+      expect($stdout).to receive(:puts).with(0)
+      missionD193.run
+    end
+  end
+end
